70194ae561da65b5b431ec60d480a5a419782c6e
Mojmir Kavan <m@ttj.cz>
2020-08-13 22:28:02 +0200

    useful c# code

---
+++ SpusteniVBS/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SpusteniVBS {
+    class Program {
+        static void Main(string[] args) {            
+            string scriptFile = "mscript.wsf";
+            System.Diagnostics.Process.Start("WScript.exe", " " + scriptFile);
+            Console.WriteLine("script ran");
+        }
+    }
+}
