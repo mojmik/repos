16a4fa290e34004a6e8d18be6c3e90a4540d378d
Mojmir Kavan <m@ttj.cz>
2020-11-29 16:34:01 +0100

    asp.net mvc basics

---
+++ MVCNahodneCislo/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCNahodneCislo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCNahodneCislo.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            Generator generator = new Generator();
+            ViewBag.Neco = "ahoj";
+            ViewBag.Cislo = generator.VratCislo();
+            return View();
+        }
+    }
+}
---
+++ MVCNahodneCislo/Models/Generator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCNahodneCislo.Models {
+    public class Generator {
+        private Random random = new Random();
+
+        public int VratCislo() {
+            return random.Next(100);
+        }
+    }
+}
